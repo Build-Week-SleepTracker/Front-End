b1f757684a43ded6d632d43bb11cdb6802dc9704
John Rojcewicz <rojcewiczj@gmail.com>
2019-09-25 15:41:18 -0500

    changes

--- sleep-tracker/src/components/SessionList.js
+++ sleep-tracker/src/components/SessionList.js
@@ -36,7 +36,7 @@ const SessionList = ({ sessions, updateSession, getData }) => {
     console.log(session);
    
      axiosWithAuth()
-      .post(`/users/sleeps`, sessionToEdit)
+      .post(`/users/sleeps/`, sessionToEdit)
       .then(res => {
         console.log(res);
         getData();
@@ -47,7 +47,7 @@ const SessionList = ({ sessions, updateSession, getData }) => {
   const deleteSession = session => {
     console.log(session);
      axiosWithAuth()
-      .delete(`/users/sleeps${session.id}`, session.id)
+      .delete(`/users/sleeps/${session.id}`, session.id)
       .then(res => {
         console.log(res);
         getData();
@@ -125,6 +125,77 @@ const SessionList = ({ sessions, updateSession, getData }) => {
             <button onClick={() => setEditing(false)}>cancel</button>
           </div>
         </form>
+        {editing && (
+            <form onSubmit={createSession}>
+            <legend>New Sleep Session</legend>
+           
+            <label>
+              Bed Time:
+              <input type="time"
+                onChange={e =>
+                  setSessionToEdit({
+                    ...sessionToEdit,
+                    bedtime:e.target.value})
+                }
+                value={sessionToEdit.bedtime}
+              />
+            </label>
+            <label>
+              Wake Time:
+              <input type="time"
+                onChange={e =>
+                  setSessionToEdit({
+                    ...sessionToEdit,
+                    waketime: e.target.value})
+                }
+                value={sessionToEdit.waketime}
+              />
+            </label>
+            <label>
+              Date:
+              <input type="date"
+                onChange={e =>
+                  setSessionToEdit({ ...sessionToEdit, date: e.target.value }) 
+                }
+                value={sessionToEdit.date}
+              />
+            </label>
+            <label>
+              Tired rating at bed time:
+              <select name="MOB">
+               <option value="">1</option>
+                <option value="">2</option>
+                <option value="">3</option>
+               <option value="">4</option>
+               </select>
+            </label>
+            <label>
+              Mood when waking:
+              <select name="MOW">
+               <option value="">1</option>
+                <option value="">2</option>
+                <option value="">3</option>
+               <option value="">4</option>
+               </select>
+              </label>
+            <label>
+              mood for day:
+              <select type = "number" name="sleepquality"   onChange={e =>
+                  setSessionToEdit({ ...sessionToEdit, sleepquality: e.target.value })} >
+               <option value="1" >1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+               <option value="4">4</option>
+               </select>
+         
+            </label>
+            <div className="button-row">
+              <button type="submit">save</button>
+              <button onClick={() => setEditing(false)}>cancel</button>
+            </div>
+          </form>
+      )} 
+      <div className="spacer" />
     <div className="session-list">
       <p>Sessions</p>
       <ul>
